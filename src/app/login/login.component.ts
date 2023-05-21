09cb02db314a05e82da06f6079e87805e3400488
ksenkso <yazunya@yandex.ru>
2023-05-21 19:30:17 +0300

    Initial commit

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent {
+  loginForm = this.fb.group({
+    username: ['', Validators.required],
+    password: ['', Validators.required],
+  });
+  
+  constructor(private fb: FormBuilder) {
+  }
+
+  login() {
+    // TODO: add auth service call
+  }
+}
