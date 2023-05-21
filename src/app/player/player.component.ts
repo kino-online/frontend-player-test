1000e1024868ffacd023d8b77a20bbb2c38f731e
ksenkso <yazunya@yandex.ru>
2023-05-21 21:33:24 +0300

    Add a player

---
+++ src/app/player/player.component.ts
@@ -0,0 +1,54 @@
+import { AfterViewInit, Component, ElementRef, HostListener, Input, NgZone, ViewChild, ViewEncapsulation } from '@angular/core';
+import videojs from 'video.js';
+import Player from 'video.js/dist/types/player';
+
+export type VideoDisplayData = {
+  sources: {
+    src: string,
+    type: string,
+  }[];
+  posterUrl?: string;
+}
+
+@Component({
+  selector: 'app-player',
+  templateUrl: './player.component.html',
+  styleUrls: [
+    './player.component.scss',
+  ],
+  encapsulation: ViewEncapsulation.None,
+  standalone: true,
+})
+export class PlayerComponent implements AfterViewInit {
+  @Input() video!: VideoDisplayData;
+  @Input() autoplay: boolean = false;
+
+  @ViewChild('video') videoElement!: ElementRef<HTMLVideoElement>;
+  player?: Player;
+
+  constructor(private zone: NgZone) { }
+
+  ngAfterViewInit(): void {
+    console.log(this.videoElement.nativeElement);
+    this.zone.runOutsideAngular(() => {
+      this.player = videojs(this.videoElement.nativeElement, {
+        autoplay: this.autoplay,
+        controls: true,
+        height: '600px',
+        sources: this.video.sources,
+        poster: this.video.posterUrl,
+      });
+    });
+  }
+
+  @HostListener('window:keyup.space')
+  togglePlay() {
+    if (!this.player) return;
+
+    if (this.player.paused()) {
+      this.player.play();
+    } else {
+      this.player.pause();
+    }
+  }
+}
