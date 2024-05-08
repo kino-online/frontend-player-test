f4fe54457cac7d589ff0ab7035515d031138f713
ort <orthodoxplus@ya.ru>
2024-05-08 11:08:09 +0300

    player - decrease width and center

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,7 @@ const source = ref('https://d2zihajmogu5jn.cloudfront.net/sintel/master.m3u8')
 </script>
 
 <template>
-  <div>
+  <div style="width: 70%; margin: 0 auto">
     <source-input v-model="source" />
 
     <app-player :source="{
